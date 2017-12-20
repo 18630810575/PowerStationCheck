7eccd12296dce1c911572f28353c943873909169
cyfsjq <2323597453@qq.com>
2017-12-20 22:11:52 +0800

    201712201011

--- PowerStationCheck/MineViewController.m
+++ PowerStationCheck/MineViewController.m
@@ -16,17 +16,29 @@
 
 @end
 
-@implementation MineViewController
+@implementation MineViewController{
+    NSArray *cellTitleArr;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"我的";
+    
+    cellTitleArr = @[@"历史任务",@"反馈",@"更改密码",@"流量上传",@"消息通知",@"清除缓存",@"关于"];
+    [self.view addSubview:self.tabelView];
+    
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    
+    
+    
+    
+    
+    
 }
 
 
@@ -41,14 +53,17 @@
 
 #pragma mark - tabview -
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    
+    return 7;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    
+    UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.textLabel.text = cellTitleArr[indexPath.row];
+    return cell;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    
+    return  60;
 }
 
 
@@ -58,25 +73,35 @@
 -(UIImageView *)headerView{
     if (!_headerView) {
         _headerView = [[UIImageView alloc]init];
-        UIImageView *avaterImg = [[UIImageView alloc]init];
-        avaterImg.image = image(@"");
-        [avaterImg makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(160 * ScreenScale);
-            make.left.equalTo((kScreenWidth - 232 * ScreenScale)/2);
-            make.width.equalTo(232 * ScreenScale);
-            make.height.equalTo(232 * ScreenScale);
+        [_headerView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.and.left.equalTo(0);
+            make.width.equalTo(kScreenWidth);
+            make.height.equalTo(576 * ScreenScale);
         }];
-        avaterImg.image = image(@"");
-        avaterImg.layer.cornerRadius = 232 / 2 * ScreenScale;
-        avaterImg.layer.borderColor = [[UIColor whiteColor]CGColor];
-        avaterImg.layer.borderWidth = 2;
-        [_headerView addSubview:avaterImg];
+//        UIImageView *avaterImg = [[UIImageView alloc]init];
+//        //avaterImg.image = image(@"");
+//        [avaterImg makeConstraints:^(MASConstraintMaker *make) {
+//            make.top.equalTo(160 * ScreenScale);
+//            make.left.equalTo((kScreenWidth - 232 * ScreenScale)/2);
+//            make.width.equalTo(232 * ScreenScale);
+//            make.height.equalTo(232 * ScreenScale);
+//        }];
+        //avaterImg.image = image(@"");
+//        avaterImg.layer.cornerRadius = 232 / 2 * ScreenScale;
+//        avaterImg.layer.borderColor = [[UIColor whiteColor]CGColor];
+//        avaterImg.layer.borderWidth = 2;
+//        [_headerView addSubview:avaterImg];
     }
     return _headerView;
 }
 -(UIButton *)headerBtn{
     if (!_headerBtn) {
         _headerBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_headerBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.and.left.equalTo(0);
+            make.width.equalTo(kScreenWidth);
+            make.height.equalTo(576 * ScreenScale);
+        }];
         [_headerBtn setBackgroundColor:[UIColor clearColor]];
         [_headerBtn setBackgroundImage:image(@"") forState:UIControlStateNormal];
         [_headerBtn addTarget:self action:@selector(changeAvater) forControlEvents:UIControlEventTouchUpInside];
@@ -87,7 +112,12 @@
 -(UITableView *)tabelView{
     if (!_tabelView) {
         _tabelView = [[UITableView alloc]init];
-        _tabelView.style
+        [_tabelView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(576 * ScreenScale);
+            make.left.equalTo(0);
+            make.width.equalTo(kScreenWidth);
+            make.height.equalTo(1222 * ScreenScale);
+        }];
         _tabelView.backgroundColor = [UIColor clearColor];
         _tabelView.separatorStyle = UITableViewCellSeparatorStyleNone;
         _tabelView.delegate =self;
@@ -101,6 +131,7 @@
         }
         _tabelView.showsVerticalScrollIndicator = YES;
     }
+    return _tabelView;
 }
 
 
