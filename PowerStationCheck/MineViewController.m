5406dd8710a1eb67936eded75766d48e49c36b8e
cyfsjq <2323597453@qq.com>
2017-12-20 21:11:29 +0800

    201712202111

--- PowerStationCheck/MineViewController.m
+++ PowerStationCheck/MineViewController.m
@@ -8,7 +8,11 @@
 
 #import "MineViewController.h"
 
-@interface MineViewController ()
+@interface MineViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic,strong)UIImageView *headerView;
+@property (nonatomic,strong)UIButton *headerBtn;
+@property (nonatomic,strong)UITableView *tabelView;
 
 @end
 
@@ -25,14 +29,86 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+#pragma mark - 私有方法 -
+-(void)changeAvater{
+    #ifdef IS_OPEN_NSLOG
+       NSLog(@"点击了头像");
+    #endif
 }
-*/
+
+
+#pragma mark - tabview -
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
+
+
+
+
+#pragma mark - getters -
+-(UIImageView *)headerView{
+    if (!_headerView) {
+        _headerView = [[UIImageView alloc]init];
+        UIImageView *avaterImg = [[UIImageView alloc]init];
+        avaterImg.image = image(@"");
+        [avaterImg makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(160 * ScreenScale);
+            make.left.equalTo((kScreenWidth - 232 * ScreenScale)/2);
+            make.width.equalTo(232 * ScreenScale);
+            make.height.equalTo(232 * ScreenScale);
+        }];
+        avaterImg.image = image(@"");
+        avaterImg.layer.cornerRadius = 232 / 2 * ScreenScale;
+        avaterImg.layer.borderColor = [[UIColor whiteColor]CGColor];
+        avaterImg.layer.borderWidth = 2;
+        [_headerView addSubview:avaterImg];
+    }
+    return _headerView;
+}
+-(UIButton *)headerBtn{
+    if (!_headerBtn) {
+        _headerBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_headerBtn setBackgroundColor:[UIColor clearColor]];
+        [_headerBtn setBackgroundImage:image(@"") forState:UIControlStateNormal];
+        [_headerBtn addTarget:self action:@selector(changeAvater) forControlEvents:UIControlEventTouchUpInside];
+     }
+    return _headerBtn;
+}
+
+-(UITableView *)tabelView{
+    if (!_tabelView) {
+        _tabelView = [[UITableView alloc]init];
+        _tabelView.style
+        _tabelView.backgroundColor = [UIColor clearColor];
+        _tabelView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        _tabelView.delegate =self;
+        _tabelView.dataSource =self;
+        _tabelView.bounces = NO;
+        if (@available(iOS 11.0, *)) {
+            _tabelView.contentInsetAdjustmentBehavior=UIScrollViewContentInsetAdjustmentNever;
+        } else {
+            // Fallback on earlier versions
+            self.automaticallyAdjustsScrollViewInsets = FALSE;
+        }
+        _tabelView.showsVerticalScrollIndicator = YES;
+    }
+}
+
+
+
+
+
+
+
+
 
 @end
