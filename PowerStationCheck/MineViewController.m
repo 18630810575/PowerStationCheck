bae01fd1bd278c4664cc963514db7deaff397428
cyfsjq <2323597453@qq.com>
2017-12-21 10:05:00 +0800

    我的页完成

--- PowerStationCheck/MineViewController.m
+++ PowerStationCheck/MineViewController.m
@@ -13,11 +13,12 @@
 @property (nonatomic,strong)UIImageView *headerView;
 @property (nonatomic,strong)UIButton *headerBtn;
 @property (nonatomic,strong)UITableView *tabelView;
-
+@property (nonatomic,strong)UIButton *registoutBtn;
 @end
 
 @implementation MineViewController{
     NSArray *cellTitleArr;
+    UIView *switchView;
 }
 
 - (void)viewDidLoad {
@@ -26,21 +27,44 @@
     
     cellTitleArr = @[@"历史任务",@"反馈",@"更改密码",@"流量上传",@"消息通知",@"清除缓存",@"关于"];
     [self.view addSubview:self.tabelView];
-    
+    [self.view addSubview:self.headerView];
+    [self.view addSubview:self.headerBtn];
+    [self.view addSubview:self.registoutBtn];
     // Do any additional setup after loading the view.
 }
 
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    
-    
-    
-    
-    
-    
-    
+    [self.tabelView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(576 * ScreenScale);
+        make.left.equalTo(0);
+        make.width.equalTo(kScreenWidth);
+        make.height.equalTo(1222 * ScreenScale);
+    }];
+    [self.headerBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.and.left.equalTo(0);
+        make.width.equalTo(kScreenWidth);
+        make.height.equalTo(576 * ScreenScale);
+    }];
+    [self.headerBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.and.left.equalTo(0);
+        make.width.equalTo(kScreenWidth);
+        make.height.equalTo(576 * ScreenScale);
+    }];
+    [self.headerView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.and.left.equalTo(0);
+        make.width.equalTo(kScreenWidth);
+        make.height.equalTo(576 * ScreenScale);
+    }];
+    [self.registoutBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(-210 * ScreenScale);
+        make.left.equalTo(60 * ScreenScale);
+        make.width.equalTo(kScreenWidth - 120 * ScreenScale);
+        make.height.equalTo(150 * ScreenScale);
+    }];
 }
-
+-(void)doNavBarSettings{
+}
 
 
 #pragma mark - 私有方法 -
@@ -49,21 +73,74 @@
        NSLog(@"点击了头像");
     #endif
 }
-
-
+-(void)switchBtnClick:(id)sender{
+    UIButton *btn = (UIButton *)sender;
+    if (btn.selected == NO) {
+        [UIView animateWithDuration:0.3 animations:^{
+            switchView.transform = CGAffineTransformMakeTranslation(50 * ScreenScale - 2, 0);
+        }];
+        
+        btn.selected = YES;
+    }else{
+        [UIView animateWithDuration:0.3 animations:^{
+            switchView.transform = CGAffineTransformIdentity;
+        }];
+        btn.selected = NO;
+    }
+}
+-(void)registout{
+    #ifdef IS_OPEN_NSLOG
+        NSLog(@"点击了注销账号");
+    #endif
+}
 #pragma mark - tabview -
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return 7;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:nil];
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
     cell.textLabel.text = cellTitleArr[indexPath.row];
+    if(indexPath.row == 3){
+        UIView *view = [[UIView alloc]init];
+        view.backgroundColor = UIColorFromRGB(0xD2D2D2);
+        [cell addSubview:view];
+        [view mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo((58 - 50 * ScreenScale)/2);
+            make.left.equalTo(kScreenWidth - 155 * ScreenScale);
+            make.width.equalTo(100 * ScreenScale);
+            make.height.equalTo(50 * ScreenScale);
+        }];
+        view.layer.cornerRadius = 25 * ScreenScale;
+        switchView = [[UIView alloc]init];
+        [view addSubview:switchView];
+        [switchView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(1);
+            make.top.equalTo(0);
+            make.width.and.height.equalTo(50 * ScreenScale);
+        }];
+        switchView.layer.cornerRadius = 25 * ScreenScale;
+        switchView.backgroundColor = [UIColor whiteColor];
+        UIButton *switchBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [cell addSubview:switchBtn];
+        [switchBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo((58 - 50 * ScreenScale)/2);
+            make.left.equalTo(kScreenWidth - 155 * ScreenScale);
+            make.width.equalTo(100 * ScreenScale);
+            make.height.equalTo(50 * ScreenScale);
+        }];
+        [switchBtn addTarget:self action:@selector(switchBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    }else if(indexPath.row == 6){
+        cell.detailTextLabel.text = [NSString stringWithFormat:@"%.1fM",21.5];
+        cell.accessoryType  = UITableViewCellAccessoryDisclosureIndicator;
+    }else{
+        cell.accessoryType  = UITableViewCellAccessoryDisclosureIndicator;
+    }
     return cell;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return  60;
+    return  58;
 }
 
 
@@ -73,35 +150,26 @@
 -(UIImageView *)headerView{
     if (!_headerView) {
         _headerView = [[UIImageView alloc]init];
-        [_headerView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.and.left.equalTo(0);
-            make.width.equalTo(kScreenWidth);
-            make.height.equalTo(576 * ScreenScale);
-        }];
-//        UIImageView *avaterImg = [[UIImageView alloc]init];
-//        //avaterImg.image = image(@"");
-//        [avaterImg makeConstraints:^(MASConstraintMaker *make) {
-//            make.top.equalTo(160 * ScreenScale);
-//            make.left.equalTo((kScreenWidth - 232 * ScreenScale)/2);
-//            make.width.equalTo(232 * ScreenScale);
-//            make.height.equalTo(232 * ScreenScale);
-//        }];
+        _headerView.image = image(@"theme_background");
+        UIImageView *avaterImg = [[UIImageView alloc]init];
         //avaterImg.image = image(@"");
-//        avaterImg.layer.cornerRadius = 232 / 2 * ScreenScale;
-//        avaterImg.layer.borderColor = [[UIColor whiteColor]CGColor];
-//        avaterImg.layer.borderWidth = 2;
-//        [_headerView addSubview:avaterImg];
-    }
+         [_headerView addSubview:avaterImg];
+        [avaterImg makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(160 * ScreenScale);
+            make.left.equalTo((kScreenWidth - 232 * ScreenScale)/2);
+            make.width.equalTo(232 * ScreenScale);
+            make.height.equalTo(232 * ScreenScale);
+        }];
+        avaterImg.image = image(@"");
+        avaterImg.layer.cornerRadius = 232 / 2 * ScreenScale;
+        avaterImg.layer.borderColor = [[UIColor whiteColor]CGColor];
+        avaterImg.layer.borderWidth = 2;
+     }
     return _headerView;
 }
 -(UIButton *)headerBtn{
     if (!_headerBtn) {
         _headerBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_headerBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.and.left.equalTo(0);
-            make.width.equalTo(kScreenWidth);
-            make.height.equalTo(576 * ScreenScale);
-        }];
         [_headerBtn setBackgroundColor:[UIColor clearColor]];
         [_headerBtn setBackgroundImage:image(@"") forState:UIControlStateNormal];
         [_headerBtn addTarget:self action:@selector(changeAvater) forControlEvents:UIControlEventTouchUpInside];
@@ -112,14 +180,8 @@
 -(UITableView *)tabelView{
     if (!_tabelView) {
         _tabelView = [[UITableView alloc]init];
-        [_tabelView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(576 * ScreenScale);
-            make.left.equalTo(0);
-            make.width.equalTo(kScreenWidth);
-            make.height.equalTo(1222 * ScreenScale);
-        }];
         _tabelView.backgroundColor = [UIColor clearColor];
-        _tabelView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        //_tabelView.separatorStyle = UITableViewCellSeparatorStyleNone;
         _tabelView.delegate =self;
         _tabelView.dataSource =self;
         _tabelView.bounces = NO;
@@ -134,7 +196,19 @@
     return _tabelView;
 }
 
-
+-(UIButton *)registoutBtn{
+    if (!_registoutBtn) {
+        _registoutBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_registoutBtn setBackgroundColor:[UIColor redColor]];
+        [_registoutBtn setTitle:@"注销账号" forState:UIControlStateNormal];
+        _registoutBtn.titleLabel.textColor = UIColorFromRGB(0xD35E58);
+        [_registoutBtn addTarget:self action:@selector(registout) forControlEvents:UIControlEventTouchUpInside];
+        _registoutBtn.layer.cornerRadius = 8 * ScreenScale;
+        _registoutBtn.alpha = 0.5;
+        
+    }
+    return _registoutBtn;
+}
 
 
 
